2daa22f91b2075491f7027d14db33ad089c9b6f9
hannah-developer <hannahchoi.developer@gmail.com>
2020-06-29 15:43:43 +0900

    function update

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let numbers = document.querySelectorAll(".number");
 let display = document.querySelector(".answer");
+let allClear = document.querySelector(".ac");
 let currentDisplay = display.innerHTML;
+display.innerText = "";
 
 numbers.forEach(function (number) {
   number.addEventListener("click", function () {
-    if (display.innerHTML.length == 1) {
-      display.innerHTML = number.innerText;
-    } else if (display.innerHTML.length < 1) {
-      display.innerHTML += number.innerText;
+    if (display.innerText == "0") {
+      display.innerText = number.innerText;
+    } else {
+      display.innerText += number.innerText;
     }
   });
+});
+
+allClear.addEventListener("click", function () {
+  display.innerText = 0;
 });
