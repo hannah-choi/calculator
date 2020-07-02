ef457dea34b9e27ec14694a0d748544b1ca4abfc
hannah-developer <hannahchoi.developer@gmail.com>
2020-07-02 17:01:17 +0900

    switch()

--- script.js
+++ script.js
@@ -25,56 +25,85 @@ function buttonEffect(element, className) {
 
 // 숫자 버튼
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    if (display.value.length > 13) {
+function numberClick() {
+  if (display.value.length > 13) {
+    return;
+  }
+  if (display.value == "0" && number != dot) {
+    display.value = number.innerText;
+    calculation = display.value;
+  } else {
+    if (numbers == dot && display.value.includes(".")) {
       return;
-    }
-    if (display.value == "0" && numbers[i] != dot) {
-      display.value = numbers[i].innerText;
-      calculation = display.value;
+    } else if (number == dot && display.value == "") {
+      return;
     } else {
-      if (numbers[i] == dot && display.value.includes(".")) {
-        return;
-      } else if (numbers[i] == dot && display.value == "") {
-        return;
-      } else {
-        display.value += numbers[i].innerText;
-        calculation = display.value;
-      }
+      display.value += number.innerText;
+      calculation = display.value;
     }
-    operatorOnOff = false;
-    buttonEffect(numbers[i], "number-active");
-  });
+  }
+  operatorOnOff = false;
+  buttonEffect(number, "number-active");
 }
+
+// function numberClick() {
+//   for (let i = 0; i < numbers.length; i++) {
+//     numbers[i].addEventListener("click", (e) => {
+//       if (display.value.length > 13) {
+//         return;
+//       }
+//       if (display.value == "0" && e.target != dot) {
+//         display.value = e.target.innerText;
+//         calculation = display.value;
+//       } else {
+//         if (e.target == dot && display.value.includes(".")) {
+//           return;
+//         } else if (e.target == dot && display.value == "") {
+//           return;
+//         } else {
+//           display.value += e.target.innerText;
+//           calculation = display.value;
+//         }
+//       }
+//       operatorOnOff = false;
+//       buttonEffect(e.target, "number-active");
+//     });
+//   }
+// }
 
 //------------------------------------------------
 
 // 연산자 버튼
 
-function operatorClick() {
-  for (let i = 0; i < operators.length; i++) {
-    operators[i].addEventListener("click", function () {
-      if (display.value.length < 13 && operatorOnOff == false) {
-        if (display.value.length >= 1) {
-          display.value += operators[i].innerText;
-        } else if ((display.value.length = 1)) {
-          display.value += "";
-        }
-      }
-    });
+function operatorClick(e) {
+  if (display.value.length < 13 && operatorOnOff == false) {
+    if (display.value.length >= 1) {
+      display.value += e.target.innerText;
+    } else if ((display.value.length = 1)) {
+      display.value += "";
+    }
   }
 
-  buttonEffect(operators[i], "operator-active");
+  buttonEffect(e.target, "operator-active");
   operatorOnOff = true;
 }
 
-/*operators[i].classList.add("operator-active");
-    setTimeout(function () {
-      if (operators[i].classList.contains("operator-active")) {
-        operators[i].classList.remove("operator-active");
-      }
-    }, 200);*/
+// function operatorClick() {
+//   for (let i = 0; i < operators.length; i++) {
+//     operators[i].addEventListener("click", (e) => {
+//       if (display.value.length < 13 && operatorOnOff == false) {
+//         if (display.value.length >= 1) {
+//           display.value += e.target.innerText;
+//         } else if ((display.value.length = 1)) {
+//           display.value += "";
+//         }
+//       }
+
+//       buttonEffect(e.target, "operator-active");
+//       operatorOnOff = true;
+//     });
+//   }
+// }
 
 //------------------------------------------------
 
@@ -96,27 +125,29 @@ function acClick() {
 
 //------------------------------------------------
 
-equal.addEventListener("click", function () {
-  if (display.value == "") {
-    return;
-  } else {
-    display.value = eval(calculation);
-  }
+function eqClick() {
+  equal.addEventListener("click", function () {
+    if (display.value == "") {
+      return;
+    } else {
+      display.value = eval(calculation);
+    }
 
-  buttonEffect(equal, "operator-active");
-});
+    buttonEffect(equal, "operator-active");
+  });
+}
 
 // 연산자가 클릭되면 flag는 on,
 // 숫자가 클릭되면 flag는 off
 
-document.querySelector(".wrapper").addEventListener("click", (e) => {
-  console.log(e.target);
-}); // wrapper 안에 있는 자식 태그들에도 모두 적용
+// document.querySelector(".wrapper").addEventListener("click", (e) => {
+//   console.log(e.target);
+// }); // wrapper 안에 있는 자식 태그들에도 모두 적용
 
 document.querySelector(".wrapper").addEventListener("click", (e) => {
   switch (e.target.dataset.key) {
     case "number":
-      alert("number");
+      numbers.forEach(numberClick);
       break;
     case "ac":
       acClick();
@@ -124,12 +155,8 @@ document.querySelector(".wrapper").addEventListener("click", (e) => {
     case "operator":
       operatorClick();
       break;
+    case "equal":
+      eqClick();
+      break;
   }
 });
-
-/// 함수로 모두 만들고 switch 구문안에 넣을것
-
-// 연산자의 경우 배열이라 [i]가 들어가는데, 이 경우 (e.target)을 써서 호출하고
-//함수를 쓸 때는 (target)으로 바꾼다
-
-//주석이나 구분선은 최대한 자제한다
